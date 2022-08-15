701f6a55fb7e7069580f812f73287bda7c74a587
Joseph Alagi <alagijoseph6@gmail.com>
2022-08-15 20:51:45 +0100

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -55,17 +55,4 @@ export default startApp;
 // ======= EEND DO NOT EDIT ========= //
 
 
-// function sortTableByColumn(table, column, asc = true) {
-//   const dirModifier = asc ? 1 : -1;
-//   const tBody = table.tBodies[0];
-//   const rows = Array.from(tBody.querySelectorAll("tr"));
-
-//   //sort each row
-//   const sortedRows = rows.sort((a, b) => {
-//     const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-//     const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-//     return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier)
-//   });
-// }
-// sortTableByColumn(document.querySelector("table"), 1, false);
 
